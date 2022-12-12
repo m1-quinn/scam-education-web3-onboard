f9af31578f58ba0350d80d9975bf605e624e5c45
0xhttps <118100161+m1-quinn@users.noreply.github.com>
2022-12-11 17:05:09 -0800

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# scam-education-web3-onboard
- 
+Before you can run:
+
+Create a .env file in the root of the projects directory containing:
+
+```
+REACT_APP_API_KEY="<Alchemy API key>" 
+```
+
+Download all dependencies in the project directory, run:
+
+## `npm install`
+
+To start a local server run:
+
+### `npm run start`
+
+Runs the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+
+The page will reload when you make changes.\
+You may also see any lint errors in the console.
+
+To create a build, run: 
+
+### `npm run build`
+
---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <link
+    rel="stylesheet"
+    href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+    crossorigin="anonymous"/>
+    <link rel="preconnect" href="https://fonts.googleapis.com"/>
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+    <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@300&display=swap" rel="stylesheet"/>
+    <App/>
+  </React.StrictMode>
+);
